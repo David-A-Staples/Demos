8ee682325ad299169bf7b88b3d7e907fd31cc8ca
David-A-Staples <david.asselin.staples@gmail.com>
2024-12-26 15:34:50 -0500

    Update 12-26-2024
    Updated page, script, css, and images.

--- research/02-dice-roll/js/script.js
+++ research/02-dice-roll/js/script.js
@@ -1,14 +1,27 @@
 
 // Script
 
-var rollFourSide = "";
-var rollSixSide = "";
-var rollTenSide = "";
-var rollTwelveSide = "";
-var rollTwentySide = "";
+var rollFourSide = '';
+var rollSixSide = '';
+var rollEightSide = '';
+var rollTenSide = '';
+var rollTwelveSide = '';
+var rollTwentySide = '';
+
+var historyContainer = document.getElementById('historyContainer');
+var currentAnswer = '';
 
 var answerText = document.getElementById('answer-text');
+
+var dice = document.querySelectorAll('.die');
+var fourDieImage = document.getElementById('four-die-image');
+var sixDieImage = document.getElementById('six-die-image');
+var eightDieImage = document.getElementById('eight-die-image');
+var tenDieImage = document.getElementById('ten-die-image');
+var twelveDieImage = document.getElementById('twelve-die-image');
 var twentyDieImage = document.getElementById('twenty-die-image');
+
+var activeDie = '';
 
 function getRandomNumber(min, max) {
     return Math.round(Math.random() * (max-min) + min);
@@ -16,16 +29,21 @@ function getRandomNumber(min, max) {
 
 function dieRoll(chosenDie) {
     answerText.style.opacity = '0';
+
+    // Roll die image
     setTimeout(
         function() {
-            // twentyDieImage.style.transform = 'rotate(360deg)';
-            if (twentyDieImage.style.transform !== 'rotate(360deg)') {
-                twentyDieImage.style.transform = 'rotate(360deg)';
-            } else {
-                twentyDieImage.style.transform = '';
-            };
-        },
-    ),
+            dice.forEach(function(dieImage) {
+                if (dieImage.style.transform !== 'rotate(360deg)') {
+                    dieImage.style.transform = 'rotate(360deg)';
+                } else {
+                    dieImage.style.transform = '';
+                }
+            });
+        }, 0,
+    );
+
+    // Die roll, based on selected die
     setTimeout(
         function() {
             if (chosenDie == "4-Sided") {
@@ -38,6 +56,12 @@ function dieRoll(chosenDie) {
                 answerText.innerText = '';
                 var rollSixSide = getRandomNumber(1, 6);
                 answerText.innerText = rollSixSide;
+                answerText.style.opacity = '1';
+            };
+            if (chosenDie == "8-Sided") {
+                answerText.innerText = '';
+                var rollEightSide = getRandomNumber(1, 8);
+                answerText.innerText = rollEightSide;
                 answerText.style.opacity = '1';
             };
             if (chosenDie == "10-Sided") {
@@ -58,10 +82,108 @@ function dieRoll(chosenDie) {
                 answerText.innerText = rollTwentySide;
                 answerText.style.opacity = '1';
             };
-        },
-        1250,
+        }, 750,
     );
+    
+    // Add result to history
+    const paragraph = document.createElement('p');
+    let selectedDie = diceList.value;
+    setTimeout(
+        function() {
+            let currentAnswer = answerText.textContent;
+            
+            var historyEntry = document.createTextNode(selectedDie + ': ' + currentAnswer);
+
+            // Append paragraph, then result to paragraph
+            paragraph.appendChild(historyEntry);
+            historyContainer.appendChild(paragraph);
+
+            // Make history paragraphs visible
+            var allHistoryParagraphs = historyContainer.querySelectorAll('p');
+            allHistoryParagraphs.forEach(function(paragraph) {
+                paragraph.style.opacity = '1';
+            });
+
+        }, 1500,
+    );
+    
 };
+
+// Make height of .nav-wrap equal to .history
+// function setHistoryNavHeights() {
+//     var historyWrap = document.querySelector('.history-wrap');
+//     var activeHistoryHeight = historyWrap.clientHeight;
+//     var navWrap = document.querySelector('.nav-wrap');
+
+//     navWrap.style.height = activeHistoryHeight + 'px';
+// };
+
+// Toggle open history
+function historyOpen() {
+    var history = document.querySelector('.history');
+
+    if (history.style.maxHeight !== '18px') {
+        history.style.maxHeight = '18px';
+    } else {
+        history.style.maxHeight = '206px';
+    }
+
+    historyContainer.style.paddingBottom = '5px';
+    historyContainer.style.borderBottom = '1px solid #ccc';
+    historyContainer.style.marginBottom = '10px';
+};
+
+// Change dice image when die is selected
+var diceList = document.getElementById('dice');
+var selectedDie = '';
+
+diceList.addEventListener('change', function() {
+    var selectedDie = this.value;
+
+    // console.log('Selected die: ', selectedDie);
+
+    var activeDie = document.querySelector('.die.active-die');
+
+    if (selectedDie == "4-Sided") {
+        activeDie.classList.remove('active-die');
+        fourDieImage.classList.add('active-die');
+    };
+    if (selectedDie == "6-Sided") {
+        activeDie.classList.remove('active-die');
+        sixDieImage.classList.add('active-die');
+    };
+    if (selectedDie == "8-Sided") {
+        activeDie.classList.remove('active-die');
+        eightDieImage.classList.add('active-die');
+    };
+    if (selectedDie == "10-Sided") {
+        activeDie.classList.remove('active-die');
+        tenDieImage.classList.add('active-die');
+    };
+    if (selectedDie == "12-Sided") {
+        activeDie.classList.remove('active-die');
+        twelveDieImage.classList.add('active-die');
+    };
+    if (selectedDie == "20-Sided") {
+        activeDie.classList.remove('active-die');
+        twentyDieImage.classList.add('active-die');
+    };
+
+    answerText.style.opacity = '0';
+    setTimeout(
+        function() {
+            answerText.innerText = '?';
+        },
+        50,
+    );
+    setTimeout(
+        function() {
+            answerText.style.opacity = '1';
+        },
+        100,
+    );
+    
+});
 
 // function dieRoll(chosenDie) {
 //     answerText.style.opacity = '0';
